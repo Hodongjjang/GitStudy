19701b70a6dab7273c526923f42ced27ecea13de
Gi7E-36 <Gi7E-36@Gi7A-23>
2023-11-13 12:00:05 +0900

    add Banana set + get

---
+++ GitStudy/src/fruits/Banana.java
@@ -0,0 +1,19 @@
+package fruits;
+
+public class Banana {
+	int price;
+	String qty;
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void setQty(String qty) {
+		this.qty = qty;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public String getQty() {
+		return qty;
+	}
+}
