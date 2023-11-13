3a589fa9375f136f459f5fc2b9b90a6c88da524b
Gi7E-36 <Gi7E-36@Gi7A-23>
2023-11-13 11:57:15 +0900

    Apple add set

--- GitStudy/src/fruits/Apple.java
+++ GitStudy/src/fruits/Apple.java
@@ -1,5 +1,13 @@
 package fruits;
 
 public class Apple {
-
+	int price;
+	String qty;
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void setQty(String qty) {
+		this.qty = qty;
+	}
 }
